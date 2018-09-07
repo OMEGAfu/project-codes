d4e2050df3620fdf2426c55890a917b3f71257f7
OMEGAfu <30321598+OMEGAfu@users.noreply.github.com>
2018-09-08 02:52:33 +0800

    Delete 00_install-packages.R

--- 00_install-packages.R
+++
@@ -1,5 +0,0 @@
-install.packages(c("ggplot2", "viridis", "raster", "sp", "rgdal", "lubridate",
-                   "RColorBrewer", "ncdf4", "plyr", "latticeExtra", "raster", 
-                   "ggmap", "glmnet", "rgeos", "spdep", "maptools"))
-
-# http://www.rspatial.org/
